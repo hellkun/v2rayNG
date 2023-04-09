bdc2071f6cc57b5a41061b6c5f8c6825362c6338
hellkun <imrk@qq.com>
2023-04-09 16:25:00 +0800

    use Navigation Compose

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/SubSettingActivity.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/SubSettingActivity.kt
@@ -1,8 +1,13 @@
 package com.v2ray.ang.ui
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.v2ray.ang.ui.compose.SubEditActivityScreen
 import com.v2ray.ang.ui.compose.SubSettingActivityScreen
 
 class SubSettingActivity : BaseActivity() {
@@ -11,16 +16,29 @@ class SubSettingActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            SubSettingActivityScreen(
-                onBack = this::finish,
-                onEditSubscription = { subId ->
-                    val intent = Intent(this, SubEditActivity::class.java)
-                    if (subId != null) {
-                        intent.putExtra("subId", subId)
-                    }
-                    startActivity(intent)
+            val navController = rememberNavController()
+
+            NavHost(navController = navController, startDestination = "subscriptions") {
+                composable("subscriptions") {
+                    SubSettingActivityScreen(
+                        onBack = this@SubSettingActivity::finish,
+                        onEditSubscription = { subId ->
+                            navController.navigate("subscription/$subId")
+                        }
+                    )
                 }
-            )
+
+                composable("subscription/{id}", arguments = listOf(
+                    navArgument("id") {
+                        type = NavType.StringType
+                        nullable = true
+                    }
+                )) { entry ->
+                    SubEditActivityScreen(onBack = {
+                        navController.popBackStack()
+                    }, subId = entry.arguments?.getString("id"))
+                }
+            }
         }
     }
 }
