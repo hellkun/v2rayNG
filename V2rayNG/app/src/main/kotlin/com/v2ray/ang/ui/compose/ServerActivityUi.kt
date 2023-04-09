815fdbe339f3240b1af52d51c5ec810027f46fa6
hellkun <imrk@qq.com>
2023-04-10 00:38:25 +0800

    use number keyboard when inputting port

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/compose/ServerActivityUi.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/compose/ServerActivityUi.kt
@@ -3,6 +3,7 @@ package com.v2ray.ang.ui.compose
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -15,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.v2ray.ang.R
 import com.v2ray.ang.dto.EConfigType
@@ -276,9 +278,14 @@ private fun ServerConfigField(
     Column(modifier = modifier) {
         Text(text = stringResource(id = titleResId))
         TextField(
-            value = content.value, onValueChange = {
+            value = content.value,
+            onValueChange = {
                 content.value = it
-            }, modifier = textFieldModifier
+            },
+            modifier = textFieldModifier,
+            keyboardOptions = if (type == FieldContentType.Port) KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.NumberPassword,
+            ) else KeyboardOptions.Default,
         )
     }
 }
